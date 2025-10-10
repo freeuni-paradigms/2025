396d4d343fb359ad11edd2731788e7cd2dd58c3e
Andria Kilasonia <akila19@freeuni.edu.ge>
2025-10-10 14:19:13 +0400

    Andria: Upload materials for seminar 10

---
+++ seminars/andria/10/sparsestringarray.c
@@ -0,0 +1,95 @@
+#include "sparsestringarray.h"
+#include "vector.h"
+#include <string.h>
+
+void StringFree(void* elemAddr){
+    free(*(char**)elemAddr);
+}
+
+/**
+* Function: SSANew
+* ----------------
+* Constructs the sparsestringarray addressed by the first argument to
+* be of the specified length, using the specified group size to decide
+* how many groups should be used to back the implementation. You can
+* assume that arrayLength is greater than groupSize, and for simplicity you
+* can also assume that groupSize divides evenly into arrayLength.
+*/
+void SSANew(sparsestringarray* ssa, int arrayLength, int groupSize) {
+	ssa->groupSize = groupSize;
+	ssa->numGroups = arrayLength / groupSize;
+	ssa->arrayLength = 0;
+	ssa->groups = malloc(sizeof(group) * ssa->numGroups);
+	
+	for (int i = 0; i < ssa->numGroups; i++)
+	{
+		ssa->groups[i].bitmap = malloc(groupSize);
+		memset(ssa->groups[i].bitmap, 0, groupSize);
+
+		VectorNew(&ssa->groups[i].strings, sizeof(char *), StringFree, groupSize);
+	}
+}
+
+/**
+* Function: SSADispose
+* --------------------
+* Disposes of all the resources embedded within the addressed
+* sparsestringarray that have built up over the course of its
+* lifetime.
+*/
+void SSADispose(sparsestringarray* ssa) {
+	for (int i = 0; i < ssa->numGroups; i++)
+	{
+		free(ssa->groups[i].bitmap);
+		VectorDispose(&ssa->groups[i].strings);
+	}
+	free(ssa->groups);
+}
+
+/**
+* Function: SSAInsert
+* -------------------
+* Inserts the C string addressed by str into the sparsestringarray addressed
+* by ssa at the specified index. If some nonempty string already resides
+* at the specified index, then it is replaced with the new one. Note that
+* SSAInsert makes a deep copy of the string address by str.
+*/
+bool SSAInsert(sparsestringarray* ssa, int index, const char* str) {
+	int group_idx = index / ssa->groupSize;
+	int string_idx = index % ssa->groupSize;
+
+	group *our_group = &ssa->groups[group_idx];
+
+	if (our_group->bitmap[string_idx])
+	{
+		VectorReplace(&our_group->strings, str, string_idx);
+
+		return false;
+	}
+
+	VectorInsert(&our_group->strings, str, string_idx);
+	our_group->bitmap[string_idx] = true;
+
+	ssa->arrayLength++;
+	return true;
+}
+
+/**
+* Function: SSAMap
+* ----------------
+* Applies the specified mapping routine to every single index/string pair
+* (along with the specified auxiliary data). Note that the mapping routine
+* is called on behalf of all strings, both empty and nonempty.
+*/
+void SSAMap(sparsestringarray* ssa, SSAMapFunction mapfn, void* auxData) {
+	for (int i = 0; i < ssa->numGroups; i++)
+	{
+		group *curr_group = &ssa->groups[i];
+		for (int j = 0; j < ssa->groupSize; j++)
+		{
+			const char *string = *(char **)VectorNth(&curr_group->strings, j);
+			mapfn(i * ssa->groupSize + j, string, auxData);
+		}
+	}
+}
+
