c1edcc05ab491960f350dc2f3ac993e1eecdb9db
Andria Kilasonia <akila19@freeuni.edu.ge>
2025-11-07 15:48:34 +0400

    Andria: add materials for seminar 18

---
+++ seminars/andria/18/counting_array.c
@@ -0,0 +1,127 @@
+#include "counting_array.h"
+
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define GET_ELEM(a, i) ((char *)(a)->elem_arr + (i) * (a)->elem_size)
+
+// typedef struct { 
+//     void *elem_arr;
+//     int *count_arr;
+
+//     int log_len;          // number of unique elements in array
+//     int aloc_len;         // alocated max number of elements
+
+//     int elem_size;        // size of a single element
+//     CmpFn cmp_fn;
+//     FreeFn free_fn;
+// } CountingArray;
+
+void CountingArrayInit(CountingArray* a, int elem_size, CmpFn cmp_fn, FreeFn free_fn) {
+    a->aloc_len = 5;
+    a->log_len = 0;
+
+    a->elem_size = elem_size;
+    a->cmp_fn = cmp_fn;
+    a->free_fn = free_fn;
+
+    a->elem_arr = malloc(a->aloc_len * elem_size);
+    a->count_arr = malloc(a->aloc_len * sizeof(int));
+
+    assert(a->elem_arr);
+    assert(a->count_arr);
+}
+
+void CountingArrayDestroy(CountingArray* a) {
+    if (a->free_fn)
+    {
+        for (int i = 0; i < a->log_len; i++)
+            a->free_fn(GET_ELEM(a, i));
+    }
+    
+    free(a->elem_arr);
+    free(a->count_arr);
+}
+
+int CountingArraySize(CountingArray* a) {
+    return a->log_len;
+}
+
+void* CountingArrayGet(CountingArray* a, int index) {
+    if (index < 0 || index >= a->log_len)
+        return NULL;
+
+    return GET_ELEM(a, index);
+}
+
+bool CountingArrayInsert(CountingArray* a, void* elem) {
+    int i = 0;
+    for (; i < a->log_len; i++)
+    {
+        if (a->cmp_fn(elem, GET_ELEM(a, i)) == 0) {
+            a->count_arr[i]++;
+            if (a->free_fn) a->free_fn(elem);
+            return false;
+        }
+
+        if (a->cmp_fn(elem, GET_ELEM(a, i)) < 0) 
+          break;
+    }
+
+    if (a->log_len == a->aloc_len) {
+        a->aloc_len *= 2;
+
+        a->elem_arr = realloc(a->elem_arr, a->aloc_len * a->elem_size);
+        a->count_arr = realloc(a->count_arr, a->aloc_len * sizeof(int));
+
+        assert(a->elem_arr);
+        assert(a->count_arr);
+    }
+
+    if (a->log_len != 0 && i != a->log_len)
+    {
+        memmove(GET_ELEM(a, i + 1), GET_ELEM(a, i), (a->log_len - i) * a->elem_size);
+        memmove(a->count_arr + i + 1, a->count_arr + i, (a->log_len - i) * sizeof(int));
+    }
+
+    memmove(GET_ELEM(a, i), elem, a->elem_size);
+    a->count_arr[i] = 1;
+
+    a->log_len++;
+    return true;
+}
+
+void CountingArrayRemove(CountingArray* a, void* elem) {
+    for (int i = 0; i < a->log_len; i++)
+    {
+        if (a->cmp_fn(elem, GET_ELEM(a, i)) < 0)
+            return;
+
+        if (a->cmp_fn(elem, GET_ELEM(a, i)) == 0) {
+            a->count_arr[i]--;
+
+            if (a->count_arr[i] == 0) {
+                if (a->free_fn) a->free_fn(GET_ELEM(a, i));
+
+                a->log_len--;
+                if (i != a->log_len) {
+                    memmove(GET_ELEM(a, i), GET_ELEM(a, i + 1), (a->log_len - i) * a->elem_size);
+                    memmove(a->count_arr + i, a->count_arr + i + 1, (a->log_len - i) * sizeof(int));
+                }
+            }
+            
+            return;
+        }
+    }
+}
+
+void CountingArrayMerge(CountingArray* a, CountingArray* o) {
+    for (int i = 0; i < o->log_len; i++)
+    {
+        for (int j = 0; j < o->count_arr[i]; j++)
+            CountingArrayInsert(a, GET_ELEM(o, i));
+    }
+
+    CountingArrayDestroy(o);
+}
