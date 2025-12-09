93b7926257cd7d1307d572795fb5300d00637336
Andria Kilasonia <akila19@freeuni.edu.ge>
2025-12-09 15:20:01 +0400

    Andria: upload template for seminar 27

---
+++ seminars/andria/27/solution.scm
@@ -0,0 +1,19 @@
+;1
+(define (reverse_ lst))
+
+;2
+(define (sorted? lst))
+
+;3
+(define (max_ lst))
+
+(define (filter_ func lst))
+
+(define (max_divisible3 lst))
+
+;4
+(define (range k))
+
+(define (pow a n))
+
+(define (generate_powers a k))
