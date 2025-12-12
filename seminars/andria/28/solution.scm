65e884905dfd03df2525b5061fee87fe2f944667
Andria Kilasonia <akila19@freeuni.edu.ge>
2025-12-12 13:25:26 +0400

    Andria: upload template for seminar 28

---
+++ seminars/andria/28/solution.scm
@@ -0,0 +1,30 @@
+; 1
+(define (digits a)
+)
+
+(define (list_power lst)
+)
+
+(define (sum lst)
+)
+
+(define (narcisisstic? n)
+)
+
+; 2
+(define (count a lst)
+)
+
+(define (set lst)
+)
+
+(define (frequency lst)
+)
+
+; 3
+(define (multiply n lst) 
+)
+
+; 4
+(define (ksubset n lst)
+)
