d78cc00d87af267fa246e4289255558f2c288eda
Andria Kilasonia <akila19@freeuni.edu.ge>
2025-12-16 15:35:07 +0400

    Andria: upload template for seminar 29

---
+++ seminars/andria/29/solution.scm
@@ -0,0 +1,28 @@
+; reverse and duplicate list
+(define (reverseDup lst)
+)
+
+
+; a^b in O(log(n))
+(define (binpow a b)
+)
+
+
+; fibonacci
+(define (fib n)
+)
+
+
+; partition with two passes over lst. We can do with one pass with (exrecursion
+(define (partition n lst)
+)
+
+
+; is lst2 permutation of lst1
+(define (permutation? lst1 lst2)
+)
+
+
+; isWay with bfs
+(define (isWay? edges start finish)
+)
