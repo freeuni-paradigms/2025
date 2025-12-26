32e1de0f90d50cd9b3b55d37c0bc19b8d751fed8
Andria Kilasonia <akila19@freeuni.edu.ge>
2025-12-26 13:13:07 +0400

    Andria: upload template for seminar 32

---
+++ seminars/andria/32/maintenance.c
@@ -0,0 +1,11 @@
+#include "maintenance.h"
+
+#include "supervisor.h"
+
+#include <pthread.h>
+#include <semaphore.h>
+
+void SetUpServerRoom(int num_tables, int hardware_team_size, int software_team_size) 
+{
+  // Implement
+}
